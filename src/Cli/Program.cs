0704b3bdbb884340e6d4ce9629fc5a6f31a56b75
Назарій Москаленко web2025-1 <nazarmoskalenko4@gmail.com>
2026-09-05 19:45:04 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("CrossApp - практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Москаленко Назарій, група ФЕІ-11");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"OC (OSDescription) : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"OC (Environment)   : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу: {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR)  : {Environment.Version}");
+Console.WriteLine($"Runtime            : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог   : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: Замовлення (клієнт, товар, замовлення, рядок замовлення)");
